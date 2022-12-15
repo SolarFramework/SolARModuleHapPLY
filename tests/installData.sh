0ef52adca03c25697dfccd78d3a3d87edf0e0494
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2022-12-15 09:11:16 +0100

    Update artifactory URL for generic

--- tests/installData.sh
+++ tests/installData.sh
@@ -1,5 +1,5 @@
 # Download maps
-curl https://artifact.b-com.com/solar-generic-local/maps/hololens/bcomLab/mapLabA_linux_0_10_0.zip -L -o mapA.zip
+curl https://repository.solarframework.org/generic/maps/hololens/bcomLab/mapLabA_linux_0_10_0.zip -L -o mapA.zip
 unzip -o mapA.zip -d ./data/maps
 rm mapA.zip
 
